7525d99589f07bb784977279d086d4163e6a2209
Shernandez43 <shernandez43@gatech.edu>
2020-11-29 16:05:39 -0600

    dashboard working, homepage set!

--- plotlyflask_tutorial/plotlydash/dashboard.py
+++ plotlyflask_tutorial/plotlydash/dashboard.py
@@ -5,7 +5,8 @@ import dash
 import dash_table
 import dash_html_components as html
 import dash_core_components as dcc
-from .data import create_dataframe
+from .data import get_df_and_figs, make_scatter, get_df, get_fig
+from dash.dependencies import Input, Output
 from .layout import html_layout
 
 
@@ -20,46 +21,82 @@ def init_dashboard(server):
         ]
     )
 
-    # Load DataFrame
-    df = create_dataframe()
-
+    # Load starting df and figs
+    cols=['Cumulative_cases','New_cases']
+    df, fig1, fig2  = get_df_and_figs(cols)
+    metric_cols = [col for col in df.columns[1:] if col not in ['Country_code', 'Country']]
+    scat = make_scatter(df, cols)
+    starter_cols = ['Country', 'Cumulative_cases','New_cases']
+    columns = [{"name": i, "id": i} for i in starter_cols]
     # Custom HTML layout
     dash_app.index_string = html_layout
 
     # Create Layout
     dash_app.layout = html.Div(
-        children=[dcc.Graph(
-            id='histogram-graph',
-            figure={
-                'data': [{
-                    'x': df['complaint_type'],
-                    'text': df['complaint_type'],
-                    'customdata': df['key'],
-                    'name': '311 Calls by region.',
-                    'type': 'histogram'
-                }],
-                'layout': {
-                    'title': 'NYC 311 Calls category.',
-                    'height': 500,
-                    'padding': 150
-                }
-            }),
-            create_data_table(df)
+        children=[dcc.Graph(id='map1', figure=fig1),
+        dcc.Graph(id='map2', figure=fig2),
+        dcc.Graph(id='scatter', figure=scat),
+        html.Div([html.H2("Change the value in the dropdowns to see the maps change!")]),
+        html.Div([dcc.Dropdown(
+                id='xaxis-column',
+                options=[{'label': i, 'value': i} for i in metric_cols],
+                value=cols[0]
+                )], style={'width': '48%', 'display': 'inline-block'}),
+         html.Div([dcc.Dropdown(
+                id='yaxis-column',
+                options=[{'label': i, 'value': i} for i in metric_cols],
+                value=cols[1]
+                )],style={'width': '48%', 'float': 'right', 'display': 'inline-block'}),
+        # create_data_table(df, cols=df.columns)
+        dash_table.DataTable(
+            id='my-table',
+            columns=columns,
+            data=df[starter_cols].to_dict('records'),
+            page_size=300,
+        )
         ],
         id='dash-container'
     )
+
+    @dash_app.callback(
+        Output('scatter', 'figure'),
+        Output('map1', 'figure'),
+        Output('map2', 'figure'),
+        Output("my-table", "data"),
+        Output('my-table', 'columns'),
+        Input('xaxis-column', 'value'),
+        Input('yaxis-column', 'value'))
+    def update_scatter(x_col, y_col):
+        df = get_df()
+        cols = [x_col, y_col]
+        scat = make_scatter(df, cols)
+
+        cols = ['Country', x_col, y_col]
+        columns = [{"name": i, "id": i} for i in cols]
+        data = df[cols].to_dict('records')
+        map1 = get_fig(df, x_col)
+        map2 = get_fig(df, y_col)
+        return scat, map1, map2, data, columns
+
+
+
     return dash_app.server
 
 
-def create_data_table(df):
+
+
+
+def create_data_table(dq, cols):
     """Create Dash datatable from Pandas DataFrame."""
+    dq = dq[cols]
+    starter_cols = ['Country', 'Cumulative_cases','death_rate']
+    columns = [{"name": i, "id": i} for i in starter_cols]
+
     table = dash_table.DataTable(
-        id='database-table',
-        columns=[{"name": i, "id": i} for i in df.columns],
-        data=df.to_dict('records'),
+        id='table',
+        columns=columns,
+        data=dq[starter_cols].to_dict('records'),
         sort_action="native",
         sort_mode='native',
-        page_size=300
     )
     return table
-
--- plotlyflask_tutorial/plotlydash/data.py
+++ plotlyflask_tutorial/plotlydash/data.py
@@ -1,14 +1,69 @@
 """Prepare data for Plotly Dash."""
+import os
+os.system('pip install scikit-learn')
 import pandas as pd
 import numpy as np
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
 
-def create_dataframe():
-    """Create Pandas DataFrame from local CSV."""
-    df = pd.read_csv('data/311-calls.csv', parse_dates=['created'])
-    df['created'] = df['created'].dt.date
-    df.drop(columns=['incident_zip'], inplace=True)
-    num_complaints = df['complaint_type'].value_counts()
-    to_remove = num_complaints[num_complaints <= 30].index
-    df.replace(to_remove, np.nan, inplace=True)
-    return df
+def get_df_and_figs(cols=['Cumulative_cases','death_rate']):
+    df = pd.read_csv('data/data.csv')
+    fig1 = px.choropleth(df, locations="Country_code",
+                    color=cols[0], # lifeExp is a column of gapminder
+                    hover_name="Country", # column to add to hover information
+                    color_continuous_scale=px.colors.sequential.Blues,
+                   title=cols[0] + ' Across the Globe')
+    fig2 = px.choropleth(df, locations="Country_code",
+                    color=cols[1], # lifeExp is a column of gapminder
+                    hover_name="Country", # column to add to hover information
+                    color_continuous_scale=px.colors.sequential.Blues,
+                   title=cols[1] + ' Across the Globe')
+
+    return df, fig1, fig2
+
+def get_df():
+    return pd.read_csv('data/data.csv')
+
+def get_fig(df, col='Cumulative_cases'):
+    fig = px.choropleth(df, locations="Country_code",
+                    color=col, # lifeExp is a column of gapminder
+                    hover_name="Country", # column to add to hover information
+                    color_continuous_scale=px.colors.sequential.Blues,
+                   title=col + ' Across the Globe')
+    return fig
+
+
+def make_scatter(dq, cols):
+    X = dq[cols[0]]
+    Y = dq[cols[1]]
+
+    x = np.array(X).reshape((-1, 1))
+    y = np.array(Y)
+    model = LinearRegression().fit(x, y)
+    y_pred = model.predict(x)
+    r2 = r2_score(y, y_pred)
+
+    # fig1 = px.scatter(dq, x, y, trendline="ols")
+    # trendline1 = fig1.data[1] # second trace, first one is scatter
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=X, y=Y, mode='markers', text=dq['Country']))
+    fig.add_trace(go.Scatter(x=X, y=y_pred, mode='lines', name='OLS fit', text='R sqared: ' + str(r2)))
+    fig = update_fig(fig, x=cols[0], y=cols[1],
+    title=cols[1] + ' vs. ' + cols[0] + ', with an R-squared of '+ str(r2))
+
+    return fig
+
+def update_fig(fig, x='', y='', title='', legend=''):
+    fig.update_layout(
+    title=title,
+    xaxis_title=x,
+    yaxis_title=y,
+    legend_title=legend,
+    font=dict(
+        family="Courier New, monospace",
+        size=16))
+    return fig
